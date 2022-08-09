8dc41ee694f0fdd55836414ba1e9f03819e90e38
Haneef Mohammed <haneefdm@gmail.com>
2022-08-08 18:10:44 -0700

    New inplace editor for cells values

--- src/view/hexview/hex-elements.tsx
+++ src/view/hexview/hex-elements.tsx
@@ -20,7 +20,198 @@ interface IHexCell {
     onChange?: OnCellChangeFunc;
 }
 
-export function HexCellValue(props: IHexCell): JSX.Element {
+interface IHexCellState {
+    frozen: boolean;
+    value: number;
+}
+export class HexCellValue extends React.Component<IHexCell, IHexCellState> {
+    private inRange = false;
+    private val = 0;
+    private origVal = 0;
+    private static currentDOMElt: HTMLSpanElement | undefined = undefined; // createRef not working on span element
+    private static lastGoodValue = '';
+    private static newGoodValue = '';
+
+    constructor(public props: IHexCell) {
+        super(props);
+        this.updatePrivates();
+        /*
+        const [frozen] = useRecoilState<boolean>(frozenState);
+        */
+        this.state = {
+            frozen: false,
+            value: this.val
+        };
+    }
+
+    private updatePrivates() {
+        this.inRange = addrInRange(this.props.address, this.props.byteOffset);
+        this.val = this.inRange ? myGlobals.bytes[this.props.byteOffset] : -1;
+        this.origVal = this.inRange ? myGlobals.origBytes[this.props.byteOffset] : -1;
+    }
+
+    componentDidUpdate(_prevProps: IHexCell) {
+        // TODO: Need to see if we need to reset states
+        this.updatePrivates();
+    }
+
+    classNames = () => {
+        return (
+            'hex-cell hex-cell-value' +
+            (this.props.dirty || this.state.frozen ? ' hex-cell-value-dirty' : '') +
+            (this.origVal !== this.state.value ? ' hex-cell-value-changed' : '')
+        );
+    };
+
+    onValueChanged = (val: string) => {
+        val = val.trim().toLowerCase();
+        while (val.startsWith('0x')) {
+            val = val.substring(2);
+        }
+        while (val.length > 1 && val.startsWith('0')) {
+            val = val.substring(1);
+        }
+        if (val.length > 2 || val.length === 0 || /[^0-9a-f]]/.test(val)) {
+            return;
+        }
+        HexCellValue.lastGoodValue = val;
+        const intVal = parseInt(val, 16);
+        if (this.state.value !== intVal) {
+            this.val = intVal;
+            this.setState({ value: intVal });
+            myGlobals.bytes[this.props.byteOffset] = intVal;
+            if (this.props.onChange) {
+                this.props.onChange(this.props.address, this.props.byteOffset, intVal);
+            }
+        }
+    };
+
+    valueStr = () => {
+        return this.state.value >= 0 ? hexValuesLookup[(this.state.value >>> 0) & 0xff] : '~~';
+    };
+
+    editable = () => {
+        return !this.state.frozen && !myGlobals.isReadonly;
+    };
+
+    public onKeyDown(event: any) {
+        if (!this.editable()) {
+            event.preventDefault();
+            return;
+        }
+        let v: string = HexCellValue.lastGoodValue;
+        if (event.key === 'Enter' || event.key === 'Tab') {
+            if (v === '') {
+                HexCellValue.revertEditsInDOM(HexCellValue.currentDOMElt, this.valueStr());
+                return;
+            }
+            v = HexCellValue.lastGoodValue;
+        } else if (event.key !== 'Escape') {
+            if (event.key.length === 1) {
+                if (!/[0-9a-fA-f]/.test(event.key)) {
+                    event.preventDefault();
+                }
+            }
+            return;
+        } else {
+            HexCellValue.revertEditsInDOM(HexCellValue.currentDOMElt, this.valueStr());
+            return;
+        }
+        if (v) {
+            HexCellValue.newGoodValue = v;
+            setTimeout(() => {
+                this.onValueChanged(v);
+            }, 1);
+        }
+    }
+    private onKeyDownFunc = this.onKeyDown.bind(this);
+
+    private static revertEditsInDOM(cell: HTMLSpanElement | undefined, val: string) {
+        if (cell) {
+            setTimeout(() => {
+                cell.innerText = val;
+            }, 1);
+        }
+    }
+
+    private onInput(ev: any) {
+        if (!this.editable()) {
+            return;
+        }
+        // console.log('onInput: new value = ' + ev.currentTarget.innerText);
+        if (ev.currentTarget.innerText.length > 2) {
+            const el = ev.currentTarget;
+            const val = HexCellValue.lastGoodValue;
+            setTimeout(() => {
+                el.innerText = val;
+            }, 1);
+        } else {
+            HexCellValue.lastGoodValue = ev.currentTarget.innerText;
+        }
+    }
+    private onInputFunc = this.onInput.bind(this);
+    private onFocus(ev: any) {
+        if (ev.currentTarget && this.editable()) {
+            // console.log(`onFocus: new = ${ev.currentTarget.innerText}, old = ${this.valueStr()}`);
+            HexCellValue.currentDOMElt = ev.currentTarget;
+            try {
+                HexCellValue.selectItem(ev.currentTarget);
+            } catch {
+                console.log('HexCellValue.selectItem failed');
+            }
+            document.addEventListener('keydown', this.onKeyDownFunc, false);
+            HexCellValue.lastGoodValue = this.valueStr();
+            HexCellValue.newGoodValue = '';
+        }
+    }
+    private onFocusFunc = this.onFocus.bind(this);
+    private onBlur(ev: any) {
+        if (ev.currentTarget && this.editable()) {
+            // console.log('onBlur: ' + ev.currentTarget.innerText);
+            document.removeEventListener('keydown', this.onKeyDownFunc, false);
+            HexCellValue.revertEditsInDOM(
+                HexCellValue.currentDOMElt,
+                HexCellValue.newGoodValue || this.valueStr()
+            );
+            HexCellValue.currentDOMElt = undefined;
+            HexCellValue.newGoodValue = '';
+        }
+    }
+    private onBlurFunc = this.onBlur.bind(this);
+
+    static selectItem(item: any) {
+        let range, selection: any;
+        if (window.getSelection && document.createRange) {
+            selection = window.getSelection();
+            range = document.createRange();
+            range.selectNodeContents(item);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        } else if ((document as any).selection && (document.body as any).createTextRange) {
+            range = (document.body as any).createTextRange();
+            range.moveToElementText(this);
+            range.select();
+        }
+    }
+
+    render() {
+        return (
+            <span
+                tabIndex={0}
+                suppressContentEditableWarning={true}
+                contentEditable={true}
+                className={this.classNames()}
+                onFocus={this.onFocusFunc}
+                onBlur={this.onBlurFunc}
+                onInput={this.onInputFunc}
+            >
+                {this.valueStr()}
+            </span>
+        );
+    }
+}
+
+export function HexCellValue_Unused(props: IHexCell): JSX.Element {
     const inRange = addrInRange(props.address, props.byteOffset);
     const val = inRange ? myGlobals.bytes[props.byteOffset] : -1;
     const origVal = inRange ? myGlobals.origBytes[props.byteOffset] : -1;
@@ -104,19 +295,20 @@ export function HexCellValue(props: IHexCell): JSX.Element {
         return !frozen && !myGlobals.isReadonly;
     };
 
-    /*
-  return (
-    <div className={classNames()}>
-      <span style={{ border: "1px" }} onClick={editable() ? handleClick : undefined}>{valueStr()}</span>
-    </div>
-  );
-  */
+    const onChange = (ev: any) => {
+        console.log(ev.type + ' ' + Object.getOwnPropertyNames(ev).join(', '));
+        console.log(ev.type + ' ' + Object.getOwnPropertyNames(ev.nativeEvent).join(', '));
+    };
+
     return (
         <span
             tabIndex={0}
+            contentEditable={true}
+            suppressContentEditableWarning={true}
             className={classNames()}
             style={{ border: '1px' }}
             onClick={editable() ? handleClick : undefined}
+            onInput={onChange}
         >
             {valueStr()}
         </span>
