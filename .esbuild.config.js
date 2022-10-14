0e50044e2abcc43d252d588eed010c11ad526e0c
Haneef Mohammed <haneefdm@gmail.com>
2022-10-14 08:18:46 -0700

    Better 'npm watch'

--- .esbuild.config.js
+++ .esbuild.config.js
@@ -1,10 +1,17 @@
-// const svgr = require('esbuild-plugin-svgr');
-// const linaria = require('@linaria/esbuild');
 const esbuild = require('esbuild');
 
 const watch = process.argv.includes('--watch');
 const minify = !watch || process.argv.includes('--minify');
 const prod = process.env.NODE_ENV === 'production';
+
+function onRebuildReport(pkg, error, result) {
+	const ts = (new Date()).toLocaleString();
+	if (error) {
+		console.log(`${ts}: ${pkg}: watch build failed:`/*, error*/)
+	} else {
+		console.log(`${ts}: ${pkg}: watch build succeeded:`, result)
+	}
+}
 
 // Build the editor provider
 esbuild.build({
@@ -14,38 +21,13 @@ esbuild.build({
 	external: ['vscode'],
 	sourcemap: watch,
 	minify: prod,
-	watch,
-	platform: 'node',
+	watch: watch && {
+		onRebuild(error, result) {
+			onRebuildReport('Extension', error, result);
+		},
+	},	platform: 'node',
   outfile: 'dist/extension.js',
 }).catch(() => process.exit(1))
-
-/* // Build the test cases
-esbuild.build({
-  entryPoints: ['src/test/index.ts'],
-	tsconfig: "./tsconfig.json",
-  bundle: true,
-	external: ['vscode', 'mocha', 'chai'],
-	sourcemap: watch,
-	minify,
-	watch,
-	platform: 'node',
-  outfile: 'dist/test.js',
-}).catch(() => process.exit(1)) */
-
-/*
-// Build the data inspector
-esbuild.build({
-  entryPoints: ['media/data_inspector/inspector.ts'],
-	tsconfig: "./tsconfig.json",
-  bundle: true,
-	external: ['vscode'],
-	sourcemap: watch ? 'inline' : false,
-	minify,
-	watch,
-	platform: 'browser',
-  outfile: 'dist/inspector.js',
-}).catch(() => process.exit(1))
-*/
 
 // Build the webview editors
 esbuild.build({
@@ -55,8 +37,11 @@ esbuild.build({
 	external: ['vscode'],
 	sourcemap: watch ? 'inline' : false,
 	minify: prod,
-	watch,
-	platform: 'browser',
+	watch: watch && {
+		onRebuild(error, result) {
+			onRebuildReport('RTOS View', error, result);
+		},
+	},	platform: 'browser',
   outfile: 'dist/memview.js',
 	plugins: [
 		// svgr(),
